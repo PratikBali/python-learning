3e8f5276a292dd3403b7b5920a9ec378dd67f6c9
pratikbali <pratikbali96@gmail.com>
2019-04-18 19:44:27 +0530

    Update a1q3.py
    a1q3 bug fix

--- a1/a1q3.py
+++ a1/a1q3.py
@@ -2,9 +2,9 @@ def Add(no1, no2):
     return no1+no2;
 
 print("Enter Number 1");
-num1 = input();
+num1 = int(input());
 print("Enter Number 2");
-num2 = input();
+num2 = int(input());
 
 ret = Add(num1,num2);
 print("Addition: ", ret);
