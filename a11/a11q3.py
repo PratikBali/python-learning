f046fa662a74dccbcb6ef164abcd2029b9b79250
Pratik Bali <pratikbali96@gmail.com>
2019-05-22 16:11:38 +0530

    bug fix

--- a11/a11q3.py
+++ a11/a11q3.py
@@ -2,7 +2,7 @@ import modules.DirectoryChecksum
 import os
 
 def main():
-    print('execute: python DuplicateRemoval.py abc deleted_log.txt')
+    print('execute: python DuplicateRemoval.py abc > deleted_log.txt')
     os.system('python modules/DuplicateRemoval.py abc > deleted_log.txt')
 
 if __name__ == '__main__':
--- a11/a11q4.py
+++ a11/a11q4.py
@@ -2,8 +2,7 @@ import modules.DirectoryChecksum
 import os
 
 def main():
-    print('execute: python DuplicateRemovalTime.py abc deleted_log_time.txt')
-    os.system('python modules/DuplicateRemovalTime.py abc > deleted_log_time.txt')
+    os.system('python C:/private/python/python-learning/a11/modules/DuplicateRemovalTime.py C:/private/python/python-learning/a11/ABC > deleted_log_time.txt')
 
 if __name__ == '__main__':
     main()
